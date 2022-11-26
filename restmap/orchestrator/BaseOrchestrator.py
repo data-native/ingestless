f03400909b1d2e16055f0af51a2587437959b56a
ffichtenmueller <frank.winkler@crayon.com>
2022-11-26 20:23:54 +0400

    implements CRUD for edge and nodes

--- restmap/orchestrator/BaseOrchestrator.py
+++ restmap/orchestrator/BaseOrchestrator.py
@@ -2,7 +2,7 @@
 
 """
 from dataclasses import dataclass, field
-from typing import List, Dict, Tuple
+from typing import List, Dict, Tuple, Union
 from restmap.compiler.function.FunctionCompiler import FunctionDeployment
 
 
@@ -12,11 +12,18 @@ class OrchestrationNode:
     Represents an orchestrated construct and alows it to be 
     scheduled in an execution runtime 
     """
-    parent: 'OrchestrationNode'
-    children: List['OrchestrationNode']
+    name: str
+    parent: 'OrchestrationNode' = None
+    children: List['OrchestrationNode'] = field(default_factory=list)
 
 @dataclass
-class IteratorNode(OrchestrationNode):
+class IteratorNodeBase:
+    parameter: dict
+@dataclass
+class IteratorNodeDefaults:
+    pass
+@dataclass
+class IteratorNode(IteratorNodeDefaults, OrchestrationNode, IteratorNodeBase):
     """
     Potential type to handle logic of iteration over a given range of 
     parameter inputs 
@@ -27,7 +34,6 @@ class IteratorNode(OrchestrationNode):
     * 
     """
     parameter: dict
-
 @dataclass
 class Edge:
     """Stores attributes for the node"""
@@ -36,7 +42,6 @@ class Edge:
     def set_params(self, params: dict):
         # Ensure only the defined parameters can be set
         self.params = params
-
 @dataclass
 class OrchestrationGraph:
     """
@@ -44,24 +49,53 @@ class OrchestrationGraph:
     the management API to define the execution dependencies
     amongst the serverless constructs
     """
-    nodes: List[OrchestrationNode] = field(default_factory=list)
+    #TODO Switch from reference by name to some internal UID to reduce memory footprint
+    nodes: Dict[str, OrchestrationNode] = field(default_factory=dict)
     edges: Dict[Tuple,Edge] = field(default_factory=dict)
-    adjs: Dict[int, set] = field(default_factory=dict)
+    adjs: Dict[str, set] = field(default_factory=dict)
+    is_directed: bool = False
 
-    def insert(self, node: OrchestrationNode):
-        """Adds a node to the graph"""
+    def insert(self, node: Union[List[OrchestrationNode], OrchestrationNode]):
+        """Adds a node or list of nodes to the graph"""
+        if not isinstance(node, List):
+            node = [node]
         # Keep track of nodes
-        self.nodes.append(node)
-        # Relate nodes
-        self.adjs[len(self.nodes)] = set()
+        for el in node:
+            self.nodes[el.name] = el
+            # Relate nodes
+            self.adjs[el.name] = set()
+
+    def remove(self, node: str):
+        """
+        Removes a node from the graph, deleting all edges to and from
+        it within the graph
+        """
+        if node not in self.nodes:
+            return
+        else:
+            # Remove the relations in both directions
+            for out_node in self.adjs[node]:
+                self.remove_edge(node, out_node)
+                # This fails silently when the edge doesn't exist
+                self.remove_edge(out_node, node)
+            del self.nodes[node]
+            del self.adjs[node]
+                
+    def node(self, node: str) -> OrchestrationNode:
+        """Retrieves the node from the graph for inspection"""
+        try:
+            return self.nodes[node]
+        except KeyError:
+            print(f"Node {node} not present in the graph")
     
     def add_edge(self, node1: str, node2: str, params: dict={}):
         """Creates an edge between two nodes"""
         # Store attributes on the edge
-        self.edges[(node1, node2), params] 
+        self.edges[(node1, node2)] = params 
         # Insert Edge into graph
-        self.adjs[self.nodes[node1]].add(node2)
-        self.adjs[self.nodes[node2]].add(node1)
+        self.adjs[node1].add(node2)
+        if not self.is_directed:
+            self.adjs[node2].add(node1)
 
     def edge(self, node1:str, node2: str) -> Edge:
         assert all([node in self.nodes for node in [node1, node2]])
@@ -74,10 +108,18 @@ class OrchestrationGraph:
         """Updates values on the edge"""
         try:
             edge = self.edges[(node1, node2)]
-            edge
+            self.edges[(node1, node2)] = edge | params
         except KeyError:
             print("Relation between nodes {node1} and {node2} does not exist.")
 
+    def remove_edge(self, node1: str, node2: str):
+        """Removes an edge from the graph"""
+        try:
+            del self.edges[(node1, node2)]    
+            if not self.is_directed:
+                del self.edges[(node2, node1)]
+        except KeyError:
+            print(f"The edge between {node1} and {node2} does not exist, or the nodes are not registered")
 
 class BaseOrchestrator:
     """
--- tests/orchestrator/test_BaseOrchestrator.py
+++ tests/orchestrator/test_BaseOrchestrator.py
@@ -14,36 +14,83 @@ def orchestrator():
 def graph():
     return OrchestrationGraph()
 
+@pytest.fixture
+def node():
+    return OrchestrationNode('basenode', None, [] )
 class TestOrchestrator:
     """
     This tests the graph functionality underlying the orchestration 
     """
+    #TODO Test difference in directional and undirectional graphs
 
     def test_can_init_orchestrator(self):
         orchestrator = BaseOrchestrator()
         assert isinstance(orchestrator.graph, OrchestrationGraph)
         assert isinstance(orchestrator._functions, dict)
         assert len(orchestrator._functions) == 1
-    
 
 class TestOrchestrationGraph:
     
     def test_can_init_graph(self, graph: OrchestrationGraph):
-        assert graph.edges
-        assert graph.nodes
-        assert graph.adjs
+        dir_graph = OrchestrationGraph(is_directed=True)
+        undir_graph = OrchestrationGraph(is_directed=False)
+        assert dir_graph.is_directed == True
+        assert undir_graph.is_directed == False
     
-    def test_can_add_node(self, graph: OrchestrationGraph):
-        node = OrchestrationNode(None, [])
+    def test_can_add_node(self,node: OrchestrationNode, graph: OrchestrationGraph):
         graph.insert(node)
-        assert node in graph.nodes
+        assert node.name in graph.nodes
         assert len(graph.nodes) == 1
         assert len(graph.adjs) == 1
+        # Entering the same node twice will result in no addition
         graph.insert(node)
-        assert len(graph.nodes) == 2
-        assert len(graph.adjs) == 2
+        assert len(graph.nodes) == 1
+        assert len(graph.adjs) == 1
+        node2 = OrchestrationNode("secondnode")
+        graph.insert(node2)
+        assert len(graph.nodes) == 2, "Two individual nodes need to be created in the graph"
+        assert len(graph.adjs) == 2, "Two entries need to be created in the adjecency list"
     
-    def 
-        
-        
+    def test_can_get_node(self, node: OrchestrationNode, graph: OrchestrationGraph):
+        graph.insert(node)
+        retrieved_node = graph.node(node.name)
+        assert retrieved_node == node
+    
+    def test_can_remove_node(self, node: OrchestrationNode, graph: OrchestrationGraph):
+        graph.insert(node)
+        graph.remove(node.name)
+        assert len(graph.nodes) == 0
+        assert len(graph.edges) == 0
+        assert len(graph.adjs) == 0
+    
+    def test_can_add_edge(self, node: OrchestrationNode, graph: OrchestrationGraph):
+        graph.insert(node)
+        node2 = OrchestrationNode("secondnode")
+        graph.insert(node2)
+        graph.add_edge(node.name, node2.name)
+        assert len(graph.adjs[node.name]) == 1, "An adjacency entry must be added for node"
+        if graph.is_directed:
+            assert len(graph.adjs[node2.name]) == 0, "In a directed graph the inverse relation is still unset"
+        else: 
+            assert len(graph.adjs[node2.name]) == 1, "In a undirected graph but relations are added"
 
+    def test_can_remove_edge(self, node: OrchestrationNode, graph: OrchestrationGraph):
+        node2 = OrchestrationNode('second')
+        graph.insert([node, node2])
+        graph.add_edge(node.name, node2.name)
+        graph.remove_edge(node.name, node2.name)
+
+
+    def test_can_update_edge(self, node: OrchestrationNode, graph: OrchestrationGraph):
+        node2 = OrchestrationNode('second')
+        graph.insert([node, node2])
+        graph.add_edge(node.name, node2.name)
+        update_dict = {
+                'status': 'updated'
+                }
+        graph.update_edge(node.name, node2.name, update_dict)
+        assert graph.edges[(node.name, node2.name)] == update_dict, "Updates to an initial edge params should contain all elements of the update only"
+        param_addition_dict = {
+                'PreviousState': 'unset'}
+        graph.update_edge(node.name, node2.name, param_addition_dict)
+        assert graph.edges[(node.name, node2.name)] == update_dict | param_addition_dict, "Updates to an existing parametrization contain the union of the dicts. Whereby the new updates overwrite existing params"
